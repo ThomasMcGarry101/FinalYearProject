a283e8b4bb292fb29c87540866210cd87d3ad36f
ThomasMcGarry101 <t.mcgarry1@nuigalway.ie>
2015-09-12 12:05:37 +0100

    Testing first commit

---
+++ src/Testing.java
@@ -0,0 +1,13 @@
+
+public class Testing {
+
+	public Testing()
+	{
+		System.out.println("Testing first commit");
+	}
+	
+	public static void main(String args[])
+	{
+		Testing test = new Testing();
+	}
+}
