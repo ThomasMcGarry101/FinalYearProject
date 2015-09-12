9eac4f35320133070f753603ed50503a76839a8b
ThomasMcGarry101 <t.mcgarry1@nuigalway.ie>
2015-09-12 12:16:41 +0100

    CreatedNewBranch

---
+++ src/TestingNewBranches.java
@@ -0,0 +1,13 @@
+
+public class TestingNewBranches {
+
+	public TestingNewBranches()
+	{
+		System.out.println("new branch created successfully");
+	}
+	
+	public static void main(String args[])
+	{
+		TestingNewBranches brc = new TestingNewBranches();
+	}
+}
